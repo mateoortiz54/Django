f8026f1ee314d934756ad815e4e1fa4d22998e1a
Mateo <mateo.ortiz54@misena.edu.co>
2022-07-22 12:09:14 -0500

    segunda subida

--- territorio/models.py
+++ territorio/models.py
@@ -1,3 +1,11 @@
 from django.db import models
 
 # Create your models here.
+
+class Aprendiz(models.Model):
+    cedula = models.IntegerField() 
+    nombre = models.CharField(max_length=100)
+    apellido = models.CharField(max_length=100)
+
+    def __str__(self):
+        return self.nombre
